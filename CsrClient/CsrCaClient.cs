3b915b26e02a7ed42aecb6c55f30b8fa6358d499
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2023-02-16 15:33:13 +0100

    C# sample

---
+++ CsrClient/CsrCaClient.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsrClient
+{
+    public class CsrCaClient
+    {
+        public string Url { get; set; }
+
+        private HttpClient client = new();
+        private readonly AccessToken token;
+
+        public string CSREndpointURL
+        {
+            get
+            {
+                string url = Url.TrimEnd('/');
+                if (!url.EndsWith("/csr"))
+                    url += "/csr";
+                return url + '/';
+            }
+        }
+
+        public CsrCaClient(string url, AccessToken token)
+        {
+            Url = url;
+            this.token = token;
+        }
+
+        public async Task<byte[]> IssueCertificate(byte[] request)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+            ByteArrayContent requestContent = new(request);
+            //requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/pkcs10");
+            HttpResponseMessage response = await client.PostAsync(CSREndpointURL, requestContent);
+
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsByteArrayAsync();
+            else
+                throw new HttpRequestException($"Could not issue certificate on backend, HTTP Status Code was {response.StatusCode} with reason {response.ReasonPhrase}", null, response.StatusCode);
+        }
+
+    }
+}
---
+++ CsrClient/Program.cs
@@ -0,0 +1,51 @@
+﻿// 2023 (c) glueckkanja-gab AG
+// Available under the terms of the MIT License, see LICENSE
+
+using Azure.Core;
+using Azure.Identity;
+using CsrClient;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+string scepmanBaseUrl = args[0];    // Example: https://scepman.example.com
+string scepmanApiScope = args[1];   // Example: api://14a287e1-2ccd-4633-8747-4e97c002d06d  (Look up the correct GUID from your app registration scepman-api)
+
+AccessToken accessToken;
+if (args.Length > 2)
+{
+    string clientId = args[2];
+    string clientSecret = args[3];
+    string tenantId = args[4];
+    ClientSecretCredential accessCredential = new(tenantId, clientId, clientSecret);
+    accessToken = await accessCredential.GetTokenAsync(new TokenRequestContext(new[] { $"{scepmanApiScope}/.default" }));
+
+}
+else
+{
+    DefaultAzureCredential accessCredential = new(); // Tries all kinds of available credentials, see https://docs.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential?view=azure-dotnet
+    accessToken = await accessCredential.GetTokenAsync(new TokenRequestContext(new[] { scepmanApiScope }));
+}
+
+
+// Create certificate request
+ECDsa key = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+CertificateRequest request = new(
+    new X500DistinguishedName("CN=Test"),
+    key,
+    HashAlgorithmName.SHA256
+);
+byte[] baCsr = request.CreateSigningRequest();
+
+
+// Send request to SCEPMan to issue the certificate
+CsrCaClient client = new(
+    scepmanBaseUrl,
+    accessToken
+);
+byte[] certificate = await client.IssueCertificate(baCsr);
+
+// Merge certificate and private key to store as PKCS#12
+X509Certificate2 cert = new(certificate);
+cert = cert.CopyWithPrivateKey(key);
+byte[] baPfx = cert.Export(X509ContentType.Pkcs12, "password");
+File.WriteAllBytes("my-certificate.pfx", baPfx);
