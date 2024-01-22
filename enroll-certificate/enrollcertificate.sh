042e1e3087f0390bb38d2b5001ab10bfe24e97b0
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2024-01-22 14:49:56 +0100

    call CsrClient.dll

--- enroll-certificate/enrollcertificate.sh
+++ enroll-certificate/enrollcertificate.sh
@@ -4,14 +4,23 @@ echo Running in $SHELL
 
 # Install dotnet core if it is not installed
 
+$dotnetcommand = dotnet
 if ! [ -x "$(command -v dotnet)" ]; then
   wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
   chmod +x ./dotnet-install.sh
   ./dotnet-install.sh --version latest
+  $dotnetcommand = $HOME/.dotnet/dotnet
 fi
 
-# Download csrrequest
+# Download CsrClient
 
-# run csrrequest and enroll a certificate
+wget https://github.com/scepman/csr-request/releases/download/alpha-test/enroll-certificate.tgz -O enroll-certificate.tgz
+mkdir csrclient
+cd csrclient
+tar -xvf enroll-certificate.tgz
 
-# Install certificate where the WiFi profile is located
+# run CsrClient and enroll a certificate
+
+dotnet CsrClient.dll csr $1 $2
+
+# Install client certificate where the WiFi profile is located
