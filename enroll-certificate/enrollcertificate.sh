844e1fee1ab25b9d3aa6baacb78fffd062d2768b
Ben Godwin <bengodw@gmail.com>
2024-04-24 14:47:14 +1000

    basic functionality for script seems to work?

--- enroll-certificate/enrollcertificate.sh
+++ enroll-certificate/enrollcertificate.sh
@@ -14,10 +14,11 @@ APPSERVICE_URL="$1"
 ABS_CERDIR=$(readlink -f "$5")
 ABS_KEYDIR=$(readlink -f "$6")
 ABS_ROOT=$(readlink -f "$7")
+ABS_SCRIPTDIR=$(readlink -f ".")
 
 
 # Example use:
-# ./enrollcertificate.sh https://app-scepman-csz5hqanxf6cs.azurewebsites.net/ api://dae9ad68-36a4-4f19-b663-cf2f4e81c95f 0f07dac8-7064-4203-a883-e86c0f4bb98a 9b3e2dea-5dd4-4f67-b0f5-fcc0ae9af63c . . /etc/ssl/scepman-dxun-root.pem
+# ./enrollcertificate.sh https://app-scepman-csz5hqanxf6cs.azurewebsites.net/ api://dae9ad68-36a4-4f19-b663-cf2f4e81c95f 0f07dac8-7064-4203-a883-e86c0f4bb98a 9b3e2dea-5dd4-4f67-b0f5-fcc0ae9af63c . . /etc/ssl/certs/scepman-dxun-root.pem
 
 # Need to instruct customer on how to set up the special app registration
 
@@ -53,33 +54,12 @@ eval "$dotnetcommand CsrClient.dll csr $APPSERVICE_URL $2 $3 interactive $4"
 CERTNAME="my-certificate" # TODO: Change how certname is acquired. From parameters?
 # Felix says should be stored in /etc/NetworkManager/system-connections but not sure if the system-connections dir will exist by default
 openssl pkcs12 -in "$CERTNAME.pfx" -nokeys -out "$ABS_CERDIR/$CERTNAME.pem" #-nodes #-nodes bypasses private key encryption.
-openssl pkcs12 -in "$CERTNAME.pfx" -nocerts -out "$ABS_KEYDIR/$CERTNAME.key" 
+openssl pkcs12 -in "$CERTNAME.pfx" -nodes -nocerts -out "$ABS_KEYDIR/$CERTNAME.key" 
 
 # Create cronjob for mTLS renewal of certificates using renewcertificate.sh script.
 # How often should the cronjob go? Should it depnd on when the certificate is meant to expire? Or just 
 # every day or some fixed interval
 
 # TODO work out where to store renewal script
-(crontab -l ; echo @daily ./renewcertificate.sh "\"$CERTNAME\"" "\"$APPSERVICE_URL\"" "\"$ABS_CERDIR\"" "\"$ABS_KEYDIR\"" "\"$ABS_ROOT\"") | crontab -
-(crontab -l ; echo @reboot ./renewcertificate.sh "\"$CERTNAME\"" "\"$APPSERVICE_URL\"" "\"$ABS_CERDIR\"" "\"$ABS_KEYDIR\"" "\"$ABS_ROOT\"") | crontab -
-
-# Attempt to make user level anacron from https://askubuntu.com/questions/235089/how-can-i-run-anacron-in-user-mode
-# mkdir -p ~/.anacron/{etc,spool,daily}
-# cd ..
-# cp ./renewcertificate.sh ~/.anacron/daily
-# cat > ~/.anacron/etc/anacrontab << EOL
-# # /etc/anacrontab: configuration file for anacron
-
-# # See anacron(8) and anacrontab(5) for details.
-
-# SHELL=/bin/bash
-# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$HOME/.anacron/daily
-
-# # period  delay  job-identifier     command
-# 1         10     renewcertificate  renewcertificate.sh
-# 1         10     test               echo "anacron working" > $HOME/file.txt
-# EOL
-# (crontab -l ; echo "@hourly /usr/sbin/anacron -s -t $HOME/.anacron/etc/anacrontab -S $HOME/.anacron/spool") | crontab -
-
-
-# TODO? Remove installed files?
+# (crontab -l ; echo @daily "\"$ABS_SCRIPTDIR/renewcertificate.sh\"" "\"$CERTNAME\"" "\"$APPSERVICE_URL\"" "\"$ABS_CERDIR\"" "\"$ABS_KEYDIR\"" "\"$ABS_ROOT\"") | crontab -
+(crontab -l ; echo @reboot sh -x "\"$ABS_SCRIPTDIR/renewcertificate.sh\"" "\"$CERTNAME\"" "\"$APPSERVICE_URL\"" "\"$ABS_CERDIR\"" "\"$ABS_KEYDIR\"" "\"$ABS_ROOT\"" " >> \"/mnt/c/Users/BenGodwin/OneDrive - glueckkanja-gab/Desktop/csr-request/enroll-certificate/output.txt\" 2>&1" ) | crontab -
--- enroll-certificate/renewcertificate.sh
+++ enroll-certificate/renewcertificate.sh
@@ -7,7 +7,9 @@
 # $5 = path to the root certificate (assuming it is pem encoded)
 
 # Example command: 
-# sh renewcertificate.sh my-certificate https://app-scepman-csz5hqanxf6cs.azurewebsites.net/ csrclient csrclient /etc/ssl/scepman-dxun-root.pem
+# sh renewcertificate.sh my-certificate https://app-scepman-csz5hqanxf6cs.azurewebsites.net/ . .  /etc/ssl/scepman-dxun-root.pem
+
+
 
 CERTNAME="$1"
 APPSERVICE_URL="$2"
@@ -15,9 +17,13 @@ ABS_CERDIR=`readlink -f "$3"`
 ABS_KEYDIR=`readlink -f "$4"`
 ABS_ROOT=`readlink -f "$5"`
 
+echo "I ran" >> "/mnt/c/Users/BenGodwin/OneDrive - glueckkanja-gab/Desktop/csr-request/enroll-certificate/file.txt" 
+
 # TODO
 # Check if issuing certificate is scepman root?
 # Check entire chain of trust?
+
+pwd
 
 # if revoked then do nothing
 OCSP_STATUS=`openssl ocsp -issuer "$ABS_ROOT" -cert "$ABS_CERDIR/$CERTNAME.pem" -url "$APPSERVICE_URL/ocsp"` 
@@ -27,9 +33,9 @@ if ! [ -z "${TRIMMED_STATUS}" ]; then
         # Certificate will expire within 10 days, renew using mTLS. 
 
         # Create a CSR
-        openssl genrsa -des3 -out temporary.key 2048
+        openssl genrsa -out temporary.key 2048
         # I don't think the config is important apart from maybe the challenge password? ATM included in package.
-        openssl req -new -key temporary.key -sha256 -out temporary.csr -config openssl-ipserver.config
+        openssl req -new -key temporary.key -sha256 -out temporary.csr -config "/mnt/c/Users/BenGodwin/OneDrive - glueckkanja-gab/Desktop/csr-request/enroll-certificate/opensslconf.config" #fix this hard coding
         # Create renewed version of certificate.
         echo "-----BEGIN PKCS7-----" > "$CERTNAME.p7b"
         curl -X POST --data "@temporary.csr" -H "Content-Type: application/pkcs10" --cert "$ABS_CERDIR/$CERTNAME.pem" --key "$ABS_KEYDIR/$CERTNAME.key"  --cacert /etc/ssl/certs/ca-certificates.crt "$APPSERVICE_URL/.well-known/est/simplereenroll" >> "$CERTNAME.p7b"
